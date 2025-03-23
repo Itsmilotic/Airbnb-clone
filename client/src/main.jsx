0ee8a14cfbefccf77f4f9941f3d61c3ddb060044
Srijan Kumar <108878018+Itsmilotic@users.noreply.github.com>
2025-03-23 17:13:46 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode basename="/Airbnb-clone/">
     <BrowserRouter basename="/Airbnb-clone/">
       <App />
     </BrowserRouter>
